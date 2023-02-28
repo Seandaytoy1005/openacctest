661c71391128a11aec14e4cb1a2cdba1cd57ee25
Seandaytoy1005 <105193758+Seandaytoy1005@users.noreply.github.com>
2023-02-28 23:55:37 +0800

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -5,19 +5,18 @@
 using namespace std;
 
 int main() {
-    #pragma acc parallel
-	{
+   
 		const int alength = pow(10, 7);
 		double sum = 0;
 		double* array = new double[alength];
 
-        #pragma acc loop
+        #pragma acc kernels
 		for (int i = 0; i < alength; i++) {
 			array[i] = sin(2 * PI * i / alength);
 			sum += array[i];
 		}
-		cout << sum << endl;
-	}
+		std::cout << sum << endl;
+	
 	
 	
 	
