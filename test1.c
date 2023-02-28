7e7ee626a3643e61f9cc75b829d22e12ac12ec23
Seandaytoy1005 <105193758+Seandaytoy1005@users.noreply.github.com>
2023-02-28 23:50:39 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#ifdef _OPENACC
+#include<openacc.h>
+#endif
+int main()
+{
+#ifdef OPENACC
+    printf("Number of device :%d\n", acc_get_num_devices(acc_device_not_host));
+#else
+    printf("OpenACC is not support.\n");
+#endif
+    return;
+}
